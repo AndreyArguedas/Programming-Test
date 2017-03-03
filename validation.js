637c4ceced8f14647b4883671320f43985b0c50b
Andrey Arguedas <and12rx12@gmailcom>
2017-03-02 19:56:11 -0600

    Index and validations are working

---
+++ validation.js
@@ -0,0 +1,32 @@
+function validateDate(){
+	var d = document.getElementById("date").value;
+	if(d === ""){
+		alert("Please enter a start date");
+		return false;
+	}
+	return true;
+}
+
+function validateDays(){
+	var d = document.getElementById("days").value;
+	if(d.length === 0){
+		alert("Please enter the number of days to increment");
+		return false;
+	}
+	return true;
+}
+
+function validateCountry(){
+	var d = document.getElementById("country").value;
+	if(d.length === 0){
+		alert("Please enter your country code");
+		return false;
+	}
+	return true;
+}
+
+function validate(){
+	if(validateDate() && validateDays() && validateCountry()){
+		return true;
+	}
+}
